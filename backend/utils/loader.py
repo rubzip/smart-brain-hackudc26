bf5518043fca1870ce3bb90dc6fd0e5177693165
Rubén <74114026+rubzip@users.noreply.github.com>
2026-02-28 00:28:18 +0100

    Funcion para scrapear webs

--- backend/utils/loader.py
+++ backend/utils/loader.py
@@ -1,10 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+
+def get_webpage_text(url):
+    """Obtiene el texto de una página web dada su URL."""
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        return soup.get_text()
+    return ""
+
 def load_file() -> str:
     pass
 
 def load_pdf() -> str:
-    pass
-
-def load_html() -> str:
     pass
 
 def load_youtube() -> str:
--- backend/utils/test_cleaner.py
+++ backend/utils/test_cleaner.py
@@ -1,6 +1,4 @@
 import pytest
-import re
-import unicodedata
 
 from cleaner import clean_text
 
